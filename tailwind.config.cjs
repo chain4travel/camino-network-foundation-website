7c3097b037097d1d2f1d4c80cf297e49c9770ce8
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2023-06-13 10:08:47 +0200

    laoyut and font updates

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,5 @@
 const colors = require('tailwindcss/colors')
+const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -10,6 +11,9 @@ module.exports = {
                 secondary: "#B440FC",
                 accent: "#35e9ad",
                 caminoSky: "#B5E3FD",
+            },
+            fontFamily: {
+                sans: ["Inter", ...defaultTheme.fontFamily.sans],
             },
 		},
 	},
