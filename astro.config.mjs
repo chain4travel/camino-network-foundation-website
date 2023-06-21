426bc72d404cd0f9ef8e16b22c1456c77031856f
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2023-06-21 17:32:21 +0200

    updated project

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
+import jopSoftwarecookieconsent from "@jop-software/astro-cookieconsent";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()]
+  integrations: [tailwind(), image(), jopSoftwarecookieconsent()]
 });
